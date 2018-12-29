b9827f22160ce7fd922b60f7918da2d4b928bc50
liqingxue <li_qingxue@163.com>
2018-12-29 17:37:53 +0800

    fineReport

--- src/main/java/com/hexagon/demo/DemoApplication.java
+++ src/main/java/com/hexagon/demo/DemoApplication.java
@@ -1,16 +1,25 @@
 package com.hexagon.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @MapperScan("com.hexagon.demo.*.mapper")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
 	}
 
 }
